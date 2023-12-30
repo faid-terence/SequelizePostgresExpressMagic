917f85881072865e9d83452992832ea172c005b0
Terence Faid JABO <faidterence@outlook.com>
2023-12-30 23:39:17 +0200

    feat(roles): delete role ById

--- src/controllers/roleController.ts
+++ src/controllers/roleController.ts
@@ -81,5 +81,26 @@ export const updateRole = async (req: Request, res: Response) => {
       message: "Role Updated Successful...!",
       updatedRole,
     });
-  } catch (error) {}
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const deleteRoleById = async (req: Request, res: Response) => {
+  const roleId = req.params.id;
+
+  if (!roleId) {
+    return res.status(400).json({ message: "Invalid roleId" });
+  }
+  try {
+    const role = await db.Role.findByPk(roleId);
+    if (!role) {
+      return res.status(404).json({ message: "Role Not Found" });
+    }
+    await role.destroy();
+
+    return res.status(200).json({ message: "Role deleted successful....." });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
