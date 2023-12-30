32b70eab73a2ba9859a5417a3f08792fcb830bc2
Terence Faid JABO <faidterence@outlook.com>
2023-12-30 16:07:40 +0200

    feat: Implement token generation in login flow

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -71,8 +71,8 @@ export const loginUser = async (req: Request, res: Response) => {
     }
 
     // Generate Token
-    const token = generateToken(userExists);
-
+    const token = await generateToken(userExists);
+    console.log("Generated Token:", token);
     return res.status(200).json({ message: "Login successful", token });
   } catch (error) {
     console.error("Error during user registration:", error);
