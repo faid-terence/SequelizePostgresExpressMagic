a205cb811bed543af6006a2983eb7be015cc08a5
Terence Faid JABO <faidterence@outlook.com>
2023-12-31 13:32:40 +0200

    feat(users): Get User by Id

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -19,3 +19,23 @@ export const findAllUsers = async (req: Request, res: Response) => {
     return res.status(500).json({ message: error.message });
   }
 };
+
+export const findUserById = async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  try {
+    const user = await db.User.findOne({ where: { id: id } });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User Not Found",
+      });
+    }
+    return res.status(200).json({
+      message: "User Found.....!",
+      user,
+    });
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+};
