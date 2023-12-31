1c58dec62b233f605a86cd62de5b8d9f09bce502
Terence Faid JABO <faidterence@outlook.com>
2023-12-31 13:45:35 +0200

    feat(users): Update User Profile

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { findAllUsers, findUserById } from "../controllers/userController";
+import {
+  findAllUsers,
+  findUserById,
+  updateUser,
+} from "../controllers/userController";
 
 const router = express.Router();
 
 router.get("/", findAllUsers);
 router.get("/:id", findUserById);
+router.patch("/:id", updateUser);
 
 export default router;
