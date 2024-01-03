a1bce981775b5af2e7a545637f30cf6735819ac9
Terence Faid JABO <faidterence@outlook.com>
2024-01-03 10:24:25 +0200

    feat(user-roles):Added unassign role to user routes

--- src/routes/userRoleRoutes.ts
+++ src/routes/userRoleRoutes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   assignRoleToUser,
   getUserRoles,
+  unAssignRoleToUser,
   viewUserRoles,
 } from "../controllers/user-role.controller";
 
@@ -9,4 +10,5 @@ const router = express.Router();
 router.get("/:id/getUserReviews", getUserRoles);
 router.post("/assignRole", assignRoleToUser);
 router.get("/viewUserRoles/:userId", viewUserRoles);
+router.post("/unassign/role", unAssignRoleToUser);
 export default router;
